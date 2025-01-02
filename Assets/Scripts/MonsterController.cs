8c5a2786d49033842ca2a5bef588f2071fa9a399
khuggalsrn <ggalsrn@khu.ac.kr>
2025-01-02 14:34:53 +0900

    Update : Monsters can use a NavOffMeshLink simultaneously.

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -10,9 +10,11 @@ public class MonsterController : MonoBehaviour
     [SerializeField] bool isAttacking = false;
     [SerializeField] NavMeshAgent navMeshAgent;
     [SerializeField] float moveSpeed = 0;
-    float time = 0;
+    [SerializeField] float time = 0;
+    [SerializeField] float distance = 0;
     float AttackPower => stat.Mob.AttackPower;
-    public float MoveSpeed{
+    public float MoveSpeed
+    {
         get { return moveSpeed; }
         set { moveSpeed = value; }
     }
@@ -35,35 +37,88 @@ public class MonsterController : MonoBehaviour
         // NavMeshAgent 컴포넌트를 가져옴
         navMeshAgent = GetComponent<NavMeshAgent>();
         // 초기 속도를 설정
-        navMeshAgent.speed = MoveSpeed;
+        navMeshAgent.speed = moveSpeed;
+        navMeshAgent.updatePosition = true;
+        navMeshAgent.updateRotation = true;
+        navMeshAgent.isStopped = false;
+        UpdatePath();
     }
     void FixedUpdate()
     {
         if (stat.isDead) return;
         time += 0.02f;
-        // if (isAttacking)
-        // {
-        //     navMeshAgent.speed = 0;
-        // }
-        // else
-        // {
-        //     navMeshAgent.speed = MoveSpeed;
-        // }
-        // if (isOnRange && !isAttacking)
-        // {
-        //     isAttacking = true;
-        //     animator.SetTrigger("Attack");
-        // }
-        // float distanceToPlayer = Vector3.Distance(transform.position, PlayerStatus.Instance.transform.position);
-
-        // if (distanceToPlayer <= Range) isOnRange = true;
-        // else isOnRange = false;
-        if (time >= 0.1f)
+        if (navMeshAgent.isOnOffMeshLink) // navMeshAgent가 offmeshlink 타고있을때
         {
-            navMeshAgent.speed = MoveSpeed;
-            time = 0f;
-            UpdatePath();
+            // Off Mesh Link를 커스터마이징하여 동작
+            StartCoroutine(TraverseOffMeshLink(navMeshAgent));
         }
+
+        if (PlayerStatus.Instance != null)
+        {
+            distance = Vector3.Distance(PlayerStatus.Instance.transform.position, transform.position);
+            if (distance < 10f)
+            {
+                if (time >= 0.2f)
+                {
+                    time = 0f;
+                    navMeshAgent.speed = moveSpeed;
+                    UpdatePath();
+                }
+            }
+            else if (distance < 25f)
+            {
+                if (time >= 0.5f)
+                {
+                    time = 0f;
+                    navMeshAgent.speed = moveSpeed;
+                    UpdatePath();
+                }
+            }
+            else if (distance < 50f)
+            {
+                if (time >= 1f)
+                {
+                    time = 0f;
+                    navMeshAgent.speed = MoveSpeed;
+                    UpdatePath();
+                }
+            }
+            else
+            {
+                if (time >= 5f)
+                {
+                    navMeshAgent.speed = moveSpeed;
+                    time = 0f;
+                    navMeshAgent.speed = moveSpeed;
+                    UpdatePath();
+                }
+            }
+        }
+    }
+    IEnumerator TraverseOffMeshLink(NavMeshAgent agent)
+    {
+        OffMeshLinkData linkData = agent.currentOffMeshLinkData;
+
+        // 링크의 시작과 끝 위치
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = linkData.endPos;
+
+        float duration = 0.5f * (startPos - endPos).magnitude; // 이동 시간
+        float elapsedTime = 0f;
+        agent.CompleteOffMeshLink();
+        // 링크 이동 완료시켜버리기 = 다른 몹들 탈 수 있음.
+
+        while (elapsedTime < duration)
+        {
+            agent.transform.position = Vector3.Lerp(startPos, endPos, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // 링크 끝에 도달
+        agent.transform.position = endPos;
+
+        // agent.CompleteOffMeshLink();
     }
     void UpdatePath()
     {
@@ -74,7 +129,9 @@ public class MonsterController : MonoBehaviour
             if (navMeshAgent.enabled == true)
             {
                 // 플레이어의 위치를 목표로 설정
-                navMeshAgent.SetDestination(PlayerStatus.Instance.transform.position);
+                if(!navMeshAgent.SetDestination(PlayerStatus.Instance.transform.position)){
+                    Debug.LogWarning("SetDest is Failed");
+                }
 
                 Vector3 moveDirection = navMeshAgent.velocity; // 현재 이동 벡터
                 if (moveDirection.magnitude > 0.1f) // 이동 중일 때만 회전
@@ -110,13 +167,4 @@ public class MonsterController : MonoBehaviour
             PlayerStatus.Instance.OnDamaged(AttackPower);
         }
     }
-    // void OnBecameInvisible()
-    // {
-    //     gameObject.SetActive(false); // 화면에서 벗어나면 비활성화
-    // }
-
-    // void OnBecameVisible()
-    // {
-    //     GetComponent<Animator>();gameObject.SetActive(true); // 화면에 들어오면 다시 활성화
-    // }
 }
