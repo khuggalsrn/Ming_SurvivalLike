bc17be3fdc8e7b23602740e4f9b2e8621c4bc45e
khuggalsrn <ggalsrn@khu.ac.kr>
2025-01-02 15:40:00 +0900

    Initialize : Parent Class of SkillObject

---
+++ Assets/Scripts/SkillObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillObject : MonoBehaviour
+{
+    [SerializeField] protected ParticleSystem MonserHitEffect;
+    [SerializeField] public float FinalDamage;
+}
