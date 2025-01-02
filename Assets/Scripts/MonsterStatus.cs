02cdac7548bfdbd32dc61d0656cfb0df268e11eb
khuggalsrn <ggalsrn@khu.ac.kr>
2025-01-02 14:41:17 +0900

    Add : MaxMonsterNum Logic, Debuff Updating Logic

--- Assets/Scripts/MonsterStatus.cs
+++ Assets/Scripts/MonsterStatus.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class MonsterStatus : MonoBehaviour
@@ -9,16 +11,67 @@ public class MonsterStatus : MonoBehaviour
     [SerializeField] public MonsterInfo Mob;
     float MaxHp => Mob.MaxHp;
     List<GameObject> items => Mob.items;
-    [SerializeField] float HitPoint;
+    List<float> DefenseForType => Mob.DefenseForType;
+    [SerializeField] public float HitPoint;
     public bool isDead;
+    public Dictionary<Debuff, int> CurDebuffs = new Dictionary<Debuff, int>();
     void Start()
     {
+        foreach (Debuff debuff in Enum.GetValues(typeof(Debuff)))
+        {
+            CurDebuffs[debuff] = 0;
+        }
+        // //150초뒤에 자동으로 사라짐. Limit 개체수 유지를 위함.
+        // StartCoroutine(Erase());
         HitPoint = MaxHp;
         animator = GetComponent<Animator>();
+        foreach (Transform child in transform.GetComponentsInChildren<Transform>())
+        {
+            // SkinnedMeshRenderer 컴포넌트를 가진 경우
+            SkinnedMeshRenderer skinnedMeshRenderer = child.GetComponent<SkinnedMeshRenderer>();
+            if (skinnedMeshRenderer != null)
+            {
+                // Renderer Opt 스크립트 추가 (이미 있다면 중복 추가 방지)
+                if (child.gameObject.GetComponent<RendererOpt>() == null)
+                {
+                    child.gameObject.AddComponent<RendererOpt>();
+                }
+                else
+                {
+                }
+            }
+        }
     }
-    public void OnDamaged(float dmg)
+    void FixedUpdate(){
+        if (CurDebuffs[Debuff.Exposed]==1){
+            PlayerStatus.Instance.transform.LookAt(new Vector3(transform.position.x,PlayerStatus.Instance.transform.position.y, transform.position.z));
+        }
+    }
+    public bool GetDebuff(Debuff debuff, DamageType type, int times, float dmg = 1f)
     {
-        Debug.Log("몬스터 피격" + dmg);
+        bool ReturnValue = false;
+        CurDebuffs[debuff] += times;
+        ReturnValue = SkillManager.Instance.Bleeding(gameObject, ref dmg, DefenseForType[(int)type], ref CurDebuffs, ref HitPoint);
+        if (HitPoint <= 0 && !isDead)
+        {
+            isDead = true;
+            animator.SetTrigger("Die");
+            Die();
+        }
+        else
+        {
+            animator.SetTrigger("Damaged");
+        }
+
+        return ReturnValue;
+    }
+    public void OnDamaged(float dmg, DamageType type)
+    {
+        SkillManager.Instance.RangeAddiDamage(transform, ref dmg);
+        SkillManager.Instance.FocusAddiDamage(ref CurDebuffs, ref dmg);
+
+        dmg = dmg * 100f / (100f + DefenseForType[(int)type]);
+        Debug.Log("몬스터가 피해를 입음" + dmg + type.ToString());
         HitPoint -= dmg;
         if (HitPoint <= 0 && !isDead)
         {
@@ -26,11 +79,10 @@ public class MonsterStatus : MonoBehaviour
             animator.SetTrigger("Die");
             Die();
         }
-        else{
+        else
+        {
             animator.SetTrigger("Damaged");
         }
-        // gameObject.layer = LayerMask.NameToLayer("Immortal");
-        // StartCoroutine(LayerBack("Object", 0.1f));
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -41,33 +93,24 @@ public class MonsterStatus : MonoBehaviour
     }
     void Die()
     {
+        GameManager.Instance.CurMonsterNum -= 1;
         gameObject.layer = LayerMask.NameToLayer("Default");
-        Destroy(GetComponent<BoxCollider>());
+        Destroy(GetComponent<CapsuleCollider>());
         Destroy(GetComponent<Rigidbody>());
         foreach (var item in items)
         {
             GameObject go = Instantiate(item, gameObject.transform);
             go.transform.SetParent(null, true);
-            // go.transform.position = new Vector3(go.transform.position.x,1,go.transform.position.z);
             int index = go.name.IndexOf("(Clone)");
             if (index > 0)
-                go.name = go.name.Substring(0, index) + MaxHp;
+                go.name = go.name.Substring(0, index);
         }
-        Destroy(gameObject,1f);
+        Destroy(gameObject, 1f);
     }
-    
-    // void OnBecameInvisible()
-    // {
-    //     Debug.Log("가려짐");
-    //     animator.enabled = false; // 화면에서 벗어나면 비활성화
-    //     transform.GetChild(1).gameObject.SetActive(false);
-    //     transform.GetChild(2).gameObject.SetActive(false);
-    // }
-
-    // void OnBecameVisible()
-    // {
-    //     animator.enabled = true; // 화면에 들어오면 다시 활성화
-    //     transform.GetChild(1).gameObject.SetActive(true);
-    //     transform.GetChild(2).gameObject.SetActive(true);
-    // }
+    IEnumerator Erase()
+    {
+        yield return new WaitForSeconds(150f);
+        GameManager.Instance.CurMonsterNum -= 1;
+        Destroy(this.gameObject);
+    }
 }
