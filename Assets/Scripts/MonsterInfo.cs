ec758d78d51084875b2078de001510a4b8f50090
khuggalsrn <ggalsrn@khu.ac.kr>
2025-01-02 14:36:18 +0900

    Add : Defense Stat

--- Assets/Scripts/MonsterInfo.cs
+++ Assets/Scripts/MonsterInfo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 [CreateAssetMenu(fileName = "NewMonster", menuName = "Monster/Monster Data")]
 public class MonsterInfo : ScriptableObject
 {
@@ -8,4 +9,5 @@ public class MonsterInfo : ScriptableObject
     public float MaxHp;
     public float AttackPower;
     public float MoveSpeed;
+    public List<float> DefenseForType = new List<float>(new float[Enum.GetValues(typeof(DamageType)).Length]);
 }
