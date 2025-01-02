2ed7fd80bf10790330b61f85150f76cb782df99b
khuggalsrn <ggalsrn@khu.ac.kr>
2025-01-02 15:14:49 +0900

    Update : The majority of the logic has been moved to the Skill Manager

--- Assets/Scripts/SwordAttack.cs
+++ Assets/Scripts/SwordAttack.cs
@@ -1,12 +1,10 @@
-using System.Buffers.Text;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Rendering;
+using System.Linq;
 using UnityEngine;
 
 public class SwordAttack : WeaponAttack
 {
-    // public ItemData Weapon;
+    // public WeaponData Weapon;
     // public SkillData skill1;
     // public SkillData skill2;
     // public ParticleSystem Basic;
@@ -14,61 +12,124 @@ public class SwordAttack : WeaponAttack
     // public SkillData FlipBurst;
     // public float cur_AttackPower = 0; // 스킬에 의한 배수
     // public float weaponbasicpower = 1; // 무기 상수
-    // public bool CanFlipBurst;
+    // public float TurnStamina;
+    // public AttackElement Element;
+    // public virtual float FinalDamage;
+    [SerializeField] Mesh UltWeapon;
+    [SerializeField] Material UltWeaponMat;
     Coroutine basiccor;
+    Animator animator => PlayerStatus.Instance.GetComponent<Animator>();
+    public override bool IsSubWeapon
+    {
+        get { return isSubWeapon; }
+        set { isSubWeapon = value; }
+    }
+    private bool isSubWeapon = false;
+    float AddiRange = 1f;
+    readonly int Skill1AttackCount = 2;
+    int AddiSkill1AttackCount = 0;
+    readonly int FlipAttackCount = 3;
+    int AddiFlipAttackCount = 0;
+    float Skill2BleedingMultiplier = 1f;
+    float Skill2_is_upgraded_zero = 1f;
     float AddPower = 0;
-    float Drain = 0f;
+    float BleedingDrain = 0f;
+    bool LvMax = false;
     bool Skill1BonusBarrior = false;
     float BonusDamage = 1f;
     float DrainStamina = 0;
     bool CanUltimate = false;
-    public SkillData Ultimate;
-    float FinalDamage => 
-            (weaponbasicpower + AddPower + PlayerStatus.Instance.Value_AdditionalWeaponPower) 
-            * cur_AttackPower 
+
+    float ReduceSubWeaponCooltime = 0f;
+    float Drain = 0f;
+    public override float FinalDamage =>
+            (weaponbasicpower + AddPower + PlayerStatus.Instance.Value_AdditionalWeaponPower)
+            * cur_AttackPower
             * PlayerStatus.Instance.Value_LastDamage
             * BonusDamage;
     protected override void Start()
     {
         base.Start();
         transform.GetChild(0).GetComponent<BoxCollider>().enabled = false;
+        Type = DamageType.Slashing;
+    }
+    private void OnDisable()
+    {
+        if (basiccor != null) StopCoroutine(basiccor);
+        PlayerStatus.Instance.GetComponent<PlayerAttack>().AttackFalse();
     }
     public override bool OnAttack(int atk)
     {
         base.OnAttack(atk);
+        float Scale = PlayerStatus.Instance.Value_AttackRange * AddiRange;
         if (basiccor != null) StopCoroutine(basiccor);
-        if (CanUltimate) StartCoroutine(UltimateEffect(Ultimate.Effect));
+        if (CanUltimate) StartCoroutine(UltimateEffect(Weapon.Ultimate.Effect, Scale));
         switch (atk)
         {
             case 0:
                 cur_AttackPower = weaponbasicpower;
-                basiccor = StartCoroutine(BasicEffect(Basic));
+                basiccor = StartCoroutine(BasicEffect(Weapon.Basic, Scale / PlayerStatus.Instance.transform.localScale.x));
                 return true;
             case 1:
-                cur_AttackPower = skill1.SkillAttackPower;
-                StartCoroutine(Skill1Effect1(skill1.Effect));
-                StartCoroutine(Skill1Effect2(skill1.Effect));
-                StartCoroutine(Skill1Effect3(skill1.Effect));
+                cur_AttackPower = Weapon.skill1.SkillAttackPower;
+                StartCoroutine(Skill1Effect(Weapon.skill1.Effect, Scale));
                 return true;
             case 2:
-                cur_AttackPower = skill2.SkillAttackPower;
-                StartCoroutine(Skill2Effect(skill2.Effect));
+                cur_AttackPower = Weapon.skill2.SkillAttackPower;
+                StartCoroutine(Skill2Effect(Weapon.skill2.Effect, Scale));
                 return true;
             case 3:
-                if (CanFlipBurst)
-                {
-                    cur_AttackPower = FlipBurst.SkillAttackPower;
-                    StartCoroutine(FlipBurstEffect(FlipBurst.Effect));
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                cur_AttackPower = Weapon.FlipBurst.SkillAttackPower;
+                StartCoroutine(FlipBurstEffect(Weapon.FlipBurst.Effect, Scale));
+                return true;
             default:
                 return false;
         }
-        
+
+    }
+    IEnumerator BasicEffect(ParticleSystem part, float scale)
+    {
+        yield return StartCoroutine(SkillManager.Instance.SwordBasicEffect(part, scale, transform, animator.GetFloat("SwordAniSpeed")));
+        basiccor = null;
+    }
+    IEnumerator Skill1Effect(ParticleSystem part, float scale)
+    {
+        yield return StartCoroutine(SkillManager.Instance.SwordSkill1Effect1(part, scale, transform, animator.GetFloat("SwordAniSpeed")));
+        if (Skill1BonusBarrior) PlayerStatus.Instance.UpBarrior(0.1f);
+        yield return StartCoroutine(SkillManager.Instance.SwordSkill1Effect2(part, scale, transform, animator.GetFloat("SwordAniSpeed")));
+        if (AddiSkill1AttackCount == 1)
+            yield return StartCoroutine(SkillManager.Instance.SwordSkill1Effect3(part, scale, transform, animator.GetFloat("SwordAniSpeed")));
+        cur_AttackPower = 1;
+        TurnStaminaAfterSkill();
+    }
+    IEnumerator Skill2Effect(ParticleSystem part, float scale)
+    {
+        float[] Abillity = new float[4] { FinalDamage * Skill2_is_upgraded_zero, FinalDamage * Skill2BleedingMultiplier, 1, BleedingDrain };
+        yield return StartCoroutine(SkillManager.Instance.SwordSkill2Effect(part, scale, Weapon, Abillity, animator.GetFloat("SwordAniSpeed")));
+        cur_AttackPower = 1;
+        TurnStaminaAfterSkill();
+    }
+    IEnumerator FlipBurstEffect(ParticleSystem part, float scale)
+    {
+        float[] Abillity = new float[4] { FinalDamage, FinalDamage, FlipAttackCount + AddiFlipAttackCount, BleedingDrain };
+        yield return StartCoroutine(SkillManager.Instance.SwordFlipBurstEffect(part, scale, Weapon, Abillity, animator.GetFloat("SwordAniSpeed")));
+        cur_AttackPower = 1;
+        TurnStaminaAfterSkill();
+    }
+    IEnumerator UltimateEffect(ParticleSystem part, float scale)
+    {
+        float[] Abillity = new float[4] { FinalDamage, FinalDamage, 1f, BleedingDrain };
+        yield return StartCoroutine(SkillManager.Instance.SwordUltimateEffect(part, scale, Weapon, Abillity, animator.GetFloat("SwordAniSpeed")));
+        cur_AttackPower = 1;
+    }
+    IEnumerator DestroyParticleSystem(ParticleSystem part, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (part != null)
+        {
+            part.Stop();
+            Destroy(part.gameObject);
+        }
     }
     public override void AttackFalse(int atk)
     {
@@ -76,152 +137,152 @@ public class SwordAttack : WeaponAttack
         // transform.GetChild(atk).GetComponent<BoxCollider>().gameObject.SetActive(false);
 
     }
-    IEnumerator BasicEffect(ParticleSystem part)
+    public override float SubWeaponAttack(int num)
     {
-        yield return new WaitForSeconds(0.4f);
-        transform.GetChild(0).gameObject.GetComponent<BoxCollider>().enabled = true;
-        yield return new WaitForSeconds(0.1f);
-        ParticleSystem temp = Instantiate(part, PlayerStatus.Instance.transform);
-        temp.transform.localScale *= PlayerStatus.Instance.Value_AttackRange / PlayerStatus.Instance.transform.localScale.x;
-        temp.transform.SetParent(PlayerStatus.Instance.transform);
-        temp.transform.position = gameObject.transform.position + PlayerStatus.Instance.transform.forward;
-        Destroy(temp.gameObject, 5f);
-        yield return new WaitForSeconds(0.3f);
-        transform.GetChild(0).gameObject.GetComponent<BoxCollider>().enabled = false;
+        Debug.Log("Sub" + num);
+        Debug.Log(PlayerStatus.Instance.WeaponsLv[num]);
+        float dmg = (weaponbasicpower + AddPower + PlayerStatus.Instance.WeaponsLv[num] * PlayerStatus.Instance.StatPerLv[0])
+            * cur_AttackPower
+            * PlayerStatus.Instance.Value_LastDamage
+            * BonusDamage;
+        GameObject temp = Instantiate(Weapon.SubWeapon, PlayerStatus.Instance.transform);
+        float[] Abillity = new float[4] { dmg, 0, 0, Drain };
+        temp.transform.position += Vector3.up * 0.2f;
+        temp.transform.localScale *= PlayerStatus.Instance.Value_AttackRange;
+        temp.GetComponent<SwordSkillObject>().InitializeParam = Abillity;
+        temp.GetComponent<SwordSkillObject>().FinalDamage = dmg;
+        temp.GetComponent<SwordSkillObject>().Drain = Drain;
+        return Weapon.SubWeaponCooltime - ReduceSubWeaponCooltime;
     }
-    IEnumerator Skill1Effect1(ParticleSystem part)
+    public override void Reset()
     {
-        Debug.Log("Eff1");
-        transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
-        ParticleSystem temp = Instantiate(part, gameObject.transform);
-        temp.transform.localScale *= PlayerStatus.Instance.Value_AttackRange / 2f;
-        Destroy(temp.gameObject, 5f);
-        yield return new WaitForSeconds(0.14f);
-        transform.GetChild(1).GetChild(0).gameObject.SetActive(false);
-    }
-    IEnumerator Skill1Effect2(ParticleSystem part)
-    {
-        Debug.Log("Eff2");
-        if(Skill1BonusBarrior) PlayerStatus.Instance.UpBarrior(0.1f);
-        yield return new WaitForSeconds(0.3f);
-        transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
-        yield return new WaitForSeconds(0.1f);
-        ParticleSystem temp = Instantiate(part, gameObject.transform);
-        temp.transform.localScale *= PlayerStatus.Instance.Value_AttackRange / 2f;
-        Destroy(temp.gameObject, 5f);
-        yield return new WaitForSeconds(0.4f);
-        transform.GetChild(1).GetChild(1).gameObject.SetActive(false);
-    }
-    IEnumerator Skill1Effect3(ParticleSystem part)
-    {
-        Debug.Log("Eff3");
-        yield return new WaitForSeconds(0.8f);
-        transform.GetChild(1).GetChild(2).gameObject.SetActive(true);
-        ParticleSystem temp = Instantiate(part, gameObject.transform);
-        temp.transform.localScale *= PlayerStatus.Instance.Value_AttackRange / 2f;
-        Destroy(temp.gameObject, 5f);
-        yield return new WaitForSeconds(0.2f);
-        transform.GetChild(1).GetChild(2).gameObject.SetActive(false);
-        cur_AttackPower = 1;
-    }
-    IEnumerator Skill2Effect(ParticleSystem part)
-    {
-        GameObject temp = Instantiate(skill2.SkillObject);
-        temp.transform.localScale = new Vector3(1, 1, 1) * PlayerStatus.Instance.Value_AttackRange;
-        temp.transform.SetLocalPositionAndRotation(PlayerStatus.Instance.transform.position, PlayerStatus.Instance.transform.rotation);
-        temp.GetComponent<SwordSkillObject>().FinalDamage = FinalDamage;
-
-        ParticleSystem temp2 = Instantiate(part, temp.transform);
-        Destroy(temp2.gameObject, 5f);
-
-        yield return new WaitForSeconds(0.2f);
-        cur_AttackPower = 1;
-        DestroyImmediate(temp);
-    }
-    IEnumerator FlipBurstEffect(ParticleSystem part)
-    {
-        GameObject temp = Instantiate(FlipBurst.SkillObject);
-        temp.transform.localScale = new Vector3(1, 1, 1) * PlayerStatus.Instance.Value_AttackRange;
-        temp.transform.SetLocalPositionAndRotation(PlayerStatus.Instance.transform.position, PlayerStatus.Instance.transform.rotation);
-        temp.GetComponent<SwordSkillObject>().FinalDamage = FinalDamage;
-
-        ParticleSystem temp2 = Instantiate(part);
-        temp2.transform.localScale *= PlayerStatus.Instance.Value_AttackRange;
-        temp2.transform.SetLocalPositionAndRotation(PlayerStatus.Instance.transform.position, PlayerStatus.Instance.transform.rotation);
-        Destroy(temp2.gameObject, 5f);
-
-        yield return new WaitForSeconds(1f);
-        cur_AttackPower = 1;
-        DestroyImmediate(temp);
-
-    }
-    IEnumerator UltimateEffect(ParticleSystem part)
-    {
-        GameObject temp = Instantiate(Ultimate.SkillObject);
-        temp.transform.localScale = new Vector3(1, 1, 1) * PlayerStatus.Instance.Value_AttackRange;
-        temp.transform.SetLocalPositionAndRotation(PlayerStatus.Instance.transform.position, PlayerStatus.Instance.transform.rotation);
-        temp.GetComponent<SwordSkillObject>().FinalDamage = FinalDamage;
-
-        ParticleSystem temp2 = Instantiate(part);
-        temp2.transform.localScale *= PlayerStatus.Instance.Value_AttackRange;
-        temp2.transform.SetLocalPositionAndRotation(PlayerStatus.Instance.transform.position, PlayerStatus.Instance.transform.rotation);
-        Destroy(temp2.gameObject, 5f);
-
-        yield return new WaitForSeconds(0.75f);
-        cur_AttackPower = 1;
-        DestroyImmediate(temp);
-    }
-    public override void Reset(){
-        
+        AddiRange = 1f;
         AddPower = 0;
-        Drain = 0f;
-        CanFlipBurst = false;
+        BleedingDrain = 0f;
+        animator.SetFloat("SwordAniSpeed", 1f);
+        AddiSkill1AttackCount = 0;
         Skill1BonusBarrior = false;
+        Skill2BleedingMultiplier = 1f;
+        Skill2_is_upgraded_zero = 1f;
+        AddiFlipAttackCount = 0;
+        TurnStamina = 0;
         BonusDamage = 1f;
         DrainStamina = 0f;
         CanUltimate = false;
+        LvMax = false;
+
+        ReduceSubWeaponCooltime = 0f;
+        Drain = 0f;
     }
     public override void LevelSet(int i)
     {
-        switch (i)
+        var p = PlayerStatus.Instance;
+        if (isSubWeapon)
         {
-            case 9:
-                CanUltimate = true;
-                goto case 8;
-            case 8:
-                AddPower += 10;
-                goto case 7;
-            case 7:
-                Drain += 1.8f;
-                goto case 6;
-            case 6:
-                DrainStamina = 1f;
-                goto case 5;
-            case 5:
-                BonusDamage = 1.2f;
-                goto case 4;
-            case 4:
-                Skill1BonusBarrior = true;
-                goto case 3;
-            case 3:
-                CanFlipBurst = true;
-                goto case 2;
-            case 2:
-                Drain += 0.2f;
-                goto case 1;
-            case 1:
-                AddPower += 2;
-                break;
+            switch (i)
+            {
+                case 4:
+                    Drain = 0.025f;
+                    goto case 3;
+                case 3:
+                    AddiRange += 0.4f;
+                    ReduceSubWeaponCooltime += 0.2f;
+                    AddPower += 2f;
+                    goto case 2;
+                case 2:
+                    ReduceSubWeaponCooltime += 0.2f;
+                    goto case 1;
+                case 1:
+                    AddiRange += 0.4f;
+                    break;
+                default:
+                    break;
+            }
         }
+        else
+            switch (i)
+            {
+                case 9:
+                    LvMax = true;
+                    goto case 8;
+                case 8:
+                    animator.SetFloat("SwordAniSpeed", 1.5f);
+                    goto case 7;
+                case 7:
+                    UltimateMode();
+                    AddiRange += 0.15f;
+                    goto case 6;
+                case 6:
+                    BleedingDrain = 0.5f;
+                    goto case 5;
+                case 5:
+                    p.WeaponsSkillLv[p.CurWeaponnum] = "1_2";
+                    Skill2BleedingMultiplier = 3f;
+                    Skill2_is_upgraded_zero = 0f;
+                    goto case 4;
+                case 4:
+                    AddiSkill1AttackCount = 1;
+                    AddiFlipAttackCount = 1;
+                    goto case 3;
+                case 3:
+                    AddiRange += 0.15f;
+                    goto case 2;
+                case 2:
+                    if (p.WeaponsSkillLv[p.CurWeaponnum].Split("_").Select(int.Parse).ToArray().Sum() < 2)
+                    {
+                        p.WeaponsSkillLv[p.CurWeaponnum] = "1_1";
+                    }
+                    AddPower += 2f;
+                    break;
+                case 1:
+                    if (p.WeaponsSkillLv[p.CurWeaponnum].Split("_").Select(int.Parse).ToArray().Sum() < 1)
+                    {
+                        p.WeaponsSkillLv[p.CurWeaponnum] = Random.Range(0, 2) == 0 ? "0_1" : "1_0";
+                    }
+                    break;
+                default:
+                    break;
+            }
     }
     protected override void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Monster"))
         {
-            PlayerStatus.Instance.HitPoint += Drain;
-            PlayerStatus.Instance.Staminas[PlayerStatus.Instance.CurWeaponnum] += DrainStamina;
-            other.gameObject.GetComponent<MonsterStatus>().OnDamaged(FinalDamage);
-            Debug.Log(FinalDamage);
+            if (cur_AttackPower == Weapon.skill1.SkillAttackPower)
+            {
+                float[] Abillity = new float[4] { FinalDamage + (FinalDamage * 0.5f * AddiSkill1AttackCount), FinalDamage, Skill1AttackCount + AddiSkill1AttackCount, BleedingDrain };
+                StartCoroutine(SkillManager.Instance.ApplyDamageOfSkill1(other.gameObject, Abillity));
+            }
+            else
+            {
+                ApplyDamage(other.gameObject);
+                ApplyDebuff(other.gameObject, 1);
+            }
         }
+    }
+    protected override void ApplyDamage(GameObject target)
+    {
+        PlayerStatus.Instance.Staminas[PlayerStatus.Instance.CurWeaponnum] += DrainStamina;
+        target.gameObject.GetComponent<MonsterStatus>().OnDamaged(FinalDamage, Type);
+    }
+    void ApplyDebuff(GameObject target, int time, float value = 1f)
+    {
+
+        if (target.gameObject.GetComponent<MonsterStatus>().GetDebuff(
+        Debuff.Lacerated, DamageType.Slashing, time, FinalDamage * value * GameManager.Instance.Debuffs[(int)Debuff.Lacerated].AddiDamageRatio))
+        {
+            SkillManager.Instance.BuffAfterBleeding(value * GameManager.Instance.Debuffs[(int)Debuff.Lacerated].AddiDamageRatio * BleedingDrain, LvMax);
+        }
+    }
+    public override void UltimateMode()
+    {
+        CanUltimate = true;
+        GetComponent<MeshFilter>().mesh = UltWeapon;
+        GetComponent<MeshRenderer>().material = UltWeaponMat;
+        transform.SetLocalPositionAndRotation(new Vector3(0, 0, 0.2f), Quaternion.Euler(-40, 90, 90));
+    }
+    void TurnStaminaAfterSkill()
+    {
+        PlayerStatus.Instance.Staminas[PlayerStatus.Instance.CurWeaponnum] += TurnStamina;
     }
 }
